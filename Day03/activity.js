2ac7f9845097c8a92386033023f77d5d8540a5df
acacia <acaciazizzo@gmail.com>
2024-02-23 14:04:43 +0800

    Finished day 3

---
+++ Day03/activity.js
@@ -0,0 +1,14 @@
+let shirt = 17;
+let shoes = 65;
+let pants = 49;
+let sunglasses = 13;
+
+let shirtNum = 2
+let shoesNum = 1
+let pantsNum = 1
+let sunglassesNum = 3
+
+let cart = shirt*shirtNum + shoes*shoesNum + pants*pantsNum + sunglasses*sunglassesNum;
+cart = cart*0.9
+console.log("Total: $", cart);
+
---
+++ Day03/data-types.js
@@ -0,0 +1,28 @@
+//primitive data types
+
+//numbers
+let score = 58;
+console.log("Type of score:", typeof score);
+console.log('Score:', score);
+//单引号和双引号都一样
+
+//string
+let PlayerName = "Emilio";
+console.log("Type of PlayerName:", typeof PlayerName);
+console.log('PlayerName:', PlayerName);
+//数值和字符串的区别是设定时是否加引号
+
+//布尔值 Boolean - Represents a value of true or false
+let isAdmin = true;
+isAdmin = false;
+console.log('The type of isAmin:', typeof isAdmin);
+
+//空值 Null - Represents intentional absence of value
+let car = null;
+console.log('The value of car is:', car);
+console.log('The type of car is:', typeof car);
+
+//Undefined: Represents unintentional absence of value
+let city;
+console.log('The value of city is:', city);
+console.log('The type of city is:', typeof city);
